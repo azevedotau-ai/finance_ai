4520c5d5192bbd7db606d19ebe1b24c9fb14d17c
Azevedo Coragem Ihemba Tau <ATau@slb.com>
2024-11-05 18:11:25 +0000

    core: add prisma and database setup

---
+++ app/subscriptions/page.tsx
@@ -0,0 +1,9 @@
+const Subscription = ()=> {
+    return(
+        <h1>Hello Sub</h1>
+    )
+}
+
+
+
+export default Subscription;
---
+++ app/transactions/page.tsx
@@ -0,0 +1,9 @@
+const Transactions = ()=> {
+    return(
+        <h1>Hello Transactions</h1>
+    )
+}
+
+
+
+export default Transactions;
